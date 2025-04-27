8fb8093f037d343eecac8518c26d869447d2f9fd
Anushma kunwar <np01cp4s230051@islingtoncollege.edu.np>
2025-04-27 16:28:58 +0545

    implement tanstack query

---
+++ mobile/providers/TanstackProvider.tsx
@@ -0,0 +1,11 @@
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { useState } from "react";
+
+const TanstackProvider = ({ children }: { children: React.ReactNode }) => {
+  const [queryClient] = useState(() => new QueryClient());
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
+
+export default TanstackProvider;
