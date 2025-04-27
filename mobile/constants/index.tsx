a369a2f8b92dff93ac3014f12a3da65d1968dbae
Anushma kunwar <np01cp4s230051@islingtoncollege.edu.np>
2025-04-27 16:28:29 +0545

    constant file

---
+++ mobile/constants/index.tsx
@@ -0,0 +1,10 @@
+export const SERVER_URL: string = "http://192.168.1.90:3333";
+
+const version: string = "/api/v1";
+export const URLS = {
+  AUTH: version + "/auths",
+  USERS: version + "/users",
+  EVENT: version + "/events",
+  SCHOOL: version + "/schools",
+  FACULTY: version + "/faculties",
+};
