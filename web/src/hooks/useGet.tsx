d7a5ab1866053bb55fed4270f882bfb65685e0f2
Anushma kunwar <np01cp4s230051@islingtoncollege.edu.np>
2025-04-27 16:42:28 +0545

    custom hooks for api

--- web/src/hooks/useGet.tsx
+++ web/src/hooks/useGet.tsx
@@ -1,12 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
 import API from "@/utils/API";
 
-const useGet = (qkey: string, urls: string, id: string) => {
+const useGet = (qkey: string, urls: string, id?: string) => {
   const { isError, isLoading, data } = useQuery({
     queryKey: [qkey, id],
     queryFn: async () => {
-      const { data } = await API.get(`${urls}/${id}`);
-      return data;
+      if (id) {
+        const { data } = await API.get(`${urls}/${id}`);
+        return data;
+      } else {
+        const { data } = await API.get(`${urls}`);
+        return data;
+      }
     },
   });
   return { isError, isLoading, data };
